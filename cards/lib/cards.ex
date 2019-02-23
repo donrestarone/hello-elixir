14948a6a8f47b92ec139d00de35fdd3bb86f9386
donrestarone <shashikejayatunge@gmail.com>
2019-02-22 23:19:12 -0500

    intro to enum module

---
+++ cards/lib/cards.ex
@@ -0,0 +1,21 @@
+defmodule Cards do
+
+  def create_deck do 
+    values = ["Ace", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do 
+      suit
+    end 
+  end
+
+  def shuffle(deck)do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+end
+# iex -S mix
+# Cards.hello()
