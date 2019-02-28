270d87e138328e7c60e3c11ff79f4ab92eb530fa
donrestarone <shashikejayatunge@gmail.com>
2019-02-28 11:09:14 -0500

    test the socket connection, message showing up on js console

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,7 +1,14 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join() do
+  def join(name, _params, socket) do
+    IO.puts("+++++++")
+    IO.puts(name)
+    IO.puts("+++++++")
+    {:ok, %{hey: "there"}, socket}
+  end
+
+  def handle_in() do
     
   end
 end
